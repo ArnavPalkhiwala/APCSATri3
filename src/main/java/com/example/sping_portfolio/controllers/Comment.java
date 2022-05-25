d26d6c2592dcdcf8cc952e04ef85f8235886f774
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2022-05-25 14:45:36 -0700

    push

--- src/main/java/com/example/sping_portfolio/controllers/Comment.java
+++ src/main/java/com/example/sping_portfolio/controllers/Comment.java
@@ -4,7 +4,7 @@ package com.example.sping_portfolio.controllers;
  * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
  */
 
-import com.example.sping_portfolio.controllers.UserComment;
+// import com.example.sping_portfolio.controllers.UserComment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+@Controller // HTTP requests are handled as a controller, using the @Controller annotation
 public class Comment {
 
     List<UserComment> luc = new ArrayList<>();
 
     @GetMapping("/comment")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does
+    // variable bindings
     public String greeting(
-            @RequestParam(name = "username", required = false, defaultValue = "First last") String username,
-            @RequestParam(name = "comment", required = false, defaultValue = "[your comment here]") String comment,
+            @RequestParam(name = "username", required = false, defaultValue = "No Username given") String username,
+            @RequestParam(name = "comment", required = false, defaultValue = "No comment given") String comment,
             Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        // @RequestParam handles required and default values, name and model are class
+        // variables, model looking like JSON
 
         UserComment uc = new UserComment(username, comment);
 
@@ -33,20 +34,19 @@ public class Comment {
         String prevUsername = "";
         luc.add(uc);
 
-        if(!(uc.GetUsername().equals(prevUsername))){
-            if(!uc.GetComment().equals("[your comment here]") && !uc.GetUsername().equals("First last")){
-                model.addAttribute("nameout", uc.GetUsername()); 
-                model.addAttribute("commentout", uc.GetComment()); 
-                model.addAttribute("dateout", uc.GetDate()); 
+        if (!(uc.GetUsername().equals(prevUsername))) {
+            if (!uc.GetComment().equals("No Username given") && !uc.GetUsername().equals("No Comment given")) {
+                model.addAttribute("nameout", uc.GetUsername());
+                model.addAttribute("commentout", uc.GetComment());
+                model.addAttribute("dateout", uc.GetDate());
             }
-        
+
         }
-       
 
         model.addAttribute("luc", luc);
 
         prevUsername = uc.GetUsername();
 
-        return "comment"; // returns HTML VIEW (greeting)
+        return "comment";
     }
 }
