d095e9ecd6d6991a23f5c330fa3a9d5b42ad5a49
Aman Jain <60991517+amanj31@users.noreply.github.com>
2022-06-01 08:17:02 -0700

    begin axios stuff

--- src/main/java/com/example/sping_portfolio/LoggedIn.java
+++ src/main/java/com/example/sping_portfolio/LoggedIn.java
@@ -1,5 +1,11 @@
 package com.example.sping_portfolio;
 
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class LoggedIn {
     public static boolean isLoggedIn = false;
 
@@ -12,6 +18,21 @@ public class LoggedIn {
     }
 
     public static LoggedIn thisLogIn = new LoggedIn();
+
+    @GetMapping("/loginAPI")
+    @ResponseBody
+    public String logInUser(@RequestParam(name = "username", required = false, defaultValue = "") String username,
+                            @RequestParam(name = "password", required = false, defaultValue = "") String password) throws IOException {
+
+        if (username.equals("admin@example.com")) {
+            if (password.equals("Password123!")) {
+                thisLogIn.changeLogIn();
+                return "Success!";
+            }
+        }
+
+        return "Login Failure";
+    }
 
 }
 
